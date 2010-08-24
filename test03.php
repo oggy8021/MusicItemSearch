bcc3e96f3280671a12b2b326459b82d54337c1d8
oggy8021 <oggyist@gmail.com>
2010-08-24 14:58:37 +0000

    git-svn-id: file:///home/katsu/svn/MusicItemSearch/trunk@6 9c94f2e9-fffd-4f0f-b879-2b1fbb836140

---
+++ test03.php
@@ -0,0 +1,15 @@
+﻿<?php
+require_once('/usr/lib/php/modules/cloudfusion/cloudfusion.class.php');
+
+$pas = new AmazonPAS();
+$pas->set_locale(PAS_LOCALE_JAPAN);
+//$res = $pas->item_search('福原美穂',array('Title' => 'Music is My Life'), PAS_LOCALE_JAPAN);
+$res = $pas->item_search('福原美穂', null, PAS_LOCALE_JAPAN);
+//$res = $pas->item_search('Music is My Life', null, PAS_LOCALE_JAPAN);
+
+//var_dump($res);
+
+print $res->body->Arguments->keywords;
+
+
+?>
