cdd9564e161fb30ad948286c5c873bf4fee27a29
oggy8021 <oggyist@gmail.com>
2010-08-25 16:50:29 +0000

    git-svn-id: file:///home/katsu/svn/MusicItemSearch/trunk@8 9c94f2e9-fffd-4f0f-b879-2b1fbb836140

--- test03.php
+++ test03.php
@@ -7,9 +7,31 @@ $pas->set_locale(PAS_LOCALE_JAPAN);
 $res = $pas->item_search('福原美穂', null, PAS_LOCALE_JAPAN);
 //$res = $pas->item_search('Music is My Life', null, PAS_LOCALE_JAPAN);
 
+
 //var_dump($res);
 
-print $res->body->Arguments->keywords;
+debugCon($res->header["date"]);
 
+//配列の何番目にkeywordsが来るかは不定
+debugCon($res->body->OperationRequest->Arguments->Argument[5]->attributes()->Name);
+
+debugCon((String) $res->body->Items->Request->ItemSearchRequest->Keywords);
+
+foreach ($res->body->Items->Item as $value)
+{
+	debugCon((String) $value->ASIN);
+}
+
+sleep(2);
+
+function debugHtml ($val)
+{
+	print "<BR><FONT COLOR=\"red\">[debug] :</FONT><strong>$val</strong><BR>\n";
+}
+
+function debugCon ($val)
+{
+	print "[debug] :$val.\n";
+}
 
 ?>
