58b02ec65ae7df8e9e5debcf639be28543b069e9
oggy8021 <oggyist@gmail.com>
2010-08-29 04:29:10 +0000

    TP 2010/8/29
    git-svn-id: file:///home/katsu/svn/MusicItemSearch/trunk@10 9c94f2e9-fffd-4f0f-b879-2b1fbb836140

---
+++ test05.php
@@ -0,0 +1,34 @@
+﻿<?php
+require_once('/usr/lib/php/modules/cloudfusion/cloudfusion.class.php');
+include_once("dBug.php");
+
+$pas = new AmazonPAS();
+$pas->set_locale(PAS_LOCALE_JAPAN);
+$opt['SearchIndex'] = 'Music';
+$opt['Keywords'] = '福原美穂';
+$opt['ResponseGroup'] = 'Images,ItemAttributes';
+$opt['Sort'] = '-releasedate';
+$res = $pas->item_search('Music', $opt, PAS_LOCALE_JAPAN);
+
+//debugCon((String) $res->body->Items->Request->ItemSearchRequest->Keywords);
+
+foreach ($res->body->Items->Item as $value)
+{
+	debugCon($value->ASIN . '-' . $value->ItemAttributes->ReleaseDate);
+}
+
+//new dBug($res);
+
+sleep(2);
+
+function debugHtml ($val)
+{
+	print "<BR><FONT COLOR=\"red\">[debug] :</FONT><strong>$val</strong><BR>\n";
+}
+
+function debugCon ($val)
+{
+	print "[debug] :$val.\n";
+}
+
+?>
